f52c1ad19c98389ecc70e497e3d7e5ef69887dce
Nicholas de Jong <62700393+nickacide@users.noreply.github.com>
2023-02-08 23:01:14 +0200

    tower setup

--- src/tower.rs
+++ src/tower.rs
@@ -1,6 +1,6 @@
-use bevy::{prelude::*, utils::FloatOrd};
-
 use crate::{bullet::*, target::Target, Lifetime};
+use bevy::{pbr::NotShadowCaster, prelude::*, utils::FloatOrd};
+use bevy_mod_picking::*;
 
 #[derive(Reflect, Component, Default)]
 #[reflect(Component)]
@@ -9,39 +9,101 @@ pub struct Tower {
     bullet_offset: Vec3,
 }
 
-pub struct TowerPlugin;
-
-impl Plugin for TowerPlugin {
-    fn build(&self, app: &mut App) {
-        app.register_type::<Tower>()
-        .add_system(tower_shooting);
-    }
-}
-
-pub fn spawn_tower(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>,
-) {
-    commands
-        .spawn(PbrBundle {
-            mesh: meshes.add(Mesh::from(shape::Cube { size: 0.25 })),
-            material: materials.add(Color::rgb(0.5, 0.5, 0.5).into()),
-            transform: Transform::from_xyz(0.0, 1.1, 0.0),
-            ..default()
-        })
-        .insert(Tower {
+impl Tower {
+    fn default() -> Self {
+        Tower {
             shooting_timer: Timer::from_seconds(1.0, TimerMode::Repeating),
             bullet_offset: Vec3 {
                 x: 0.0,
                 y: 0.0,
                 z: 0.0,
             },
-        })
-        .insert(Name::new("Tower"));
+        }
+    }
 }
 
-pub fn tower_shooting(
+pub struct TowerPlugin;
+
+impl Plugin for TowerPlugin {
+    fn build(&self, app: &mut App) {
+        app.register_type::<Tower>()
+            .add_system(tower_shooting)
+            .add_system(build_tower);
+    }
+}
+
+pub fn create_base_tower(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let default_collider_color = materials.add(Color::rgba(0.3, 0.5, 0.3, 0.3).into());
+    let selected_collider_color = materials.add(Color::rgba(0.3, 0.9, 0.3, 0.9).into());
+
+    commands
+        .spawn(SpatialBundle::from_transform(Transform::from_xyz(
+            0.0, 0.1, 0.0,
+        )))
+        // .insert(Tower::default())
+        .insert(meshes.add(shape::Cube { size: 0.25 }.into()))
+        .insert(default_collider_color.clone())
+        .insert(Highlighting {
+            initial: default_collider_color,
+            hovered: Some(selected_collider_color.clone()),
+            pressed: Some(selected_collider_color.clone()),
+            selected: Some(selected_collider_color),
+        })
+        .insert(NotShadowCaster)
+        .insert(PickableBundle::default())
+        .insert(Name::new("Base Tower"));
+}
+
+fn spawn_tower(
+    commands: &mut Commands,
+    mesh: Handle<Mesh>,
+    material: Handle<StandardMaterial>,
+    position: Vec3,
+) -> Entity {
+    commands
+        .spawn(SpatialBundle::from_transform(Transform::from_translation(
+            position,
+        )))
+        .insert(Name::new("Tower"))
+        .insert(Tower::default())
+        .insert(PbrBundle {
+            mesh,
+            material,
+            transform: Transform::from_xyz(0.0, 0.1, 0.0),
+            ..default()
+        })
+        .id()
+}
+
+fn build_tower(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+    selection: Query<(Entity, &Selection, &Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+) {
+    if keyboard.just_pressed(KeyCode::Space) {
+        for (entity, selection, transform) in &selection {
+            if selection.selected() {
+                commands.entity(entity).despawn_recursive();
+                let tower_material = materials.add(Color::rgb(0.5, 0.5, 0.5).into());
+                let tower_mesh = meshes.add(Mesh::from(shape::Cube { size: 0.25 }));
+                spawn_tower(
+                    &mut commands,
+                    tower_mesh,
+                    tower_material,
+                    transform.translation,
+                );
+            }
+        }
+    }
+}
+
+fn tower_shooting(
     mut commands: Commands,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
